09d5e0096d1f84607a9f59737330c5e312fbdc8f
mniane001 <nianemouhamed100@gmail.com>
2023-10-26 10:21:03 -0400

    Set up environnemnt variables and initialize app

---
+++ MVC/bootstrap/init.php
@@ -0,0 +1,9 @@
+<?php 
+//Start session if not already started
+if(!isset($_SESSION)) session_start();
+
+
+//load environnement variable
+require_once __DIR__ ."/../app/config/_env.php";
+
+
--- MVC/public/index.php
+++ MVC/public/index.php
@@ -1,1 +1,9 @@
-hi no dbngn
+<?php 
+
+require_once __DIR__ ."/../bootstrap/init.php";
+
+
+$app_name=getenv("APP_NAME");
+
+
+echo $app_name;
